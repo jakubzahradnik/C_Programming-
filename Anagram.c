a63c1e84f7a3ce81f98a1d17d10ee033ed9f0c90
jakubzahradnik <279689@uwr.edu.pl>
2015-10-29 12:35:38 +0100

    Update Anagram.c

--- Anagram.c
+++ Anagram.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include <string.h>
-#include <stdio.h>
 
 #define MAX 1000
 int check_anagram(char a[], char b[])
@@ -36,7 +34,7 @@ int main()
 {
     char a[MAX], b[MAX];
     int flaga;
-    int bufor1,bufor2;
+
     int t;
     scanf("%d\n", &t);
 
@@ -47,16 +45,17 @@ int main()
                     fgets(a,MAX,stdin);
                     fgets(b,MAX,stdin);
                     flaga = check_anagram(a, b);
-                    bufor1=flaga;
-                    bufor2=bufor1;
-                    bufor1=flaga;
+                    
+                    if (flaga == 1) printf("TAK\n");
+                    else printf("NIE\n");
                     t--;
+
                 }
         }
-        if (bufor1 == 1) printf("TAK\n");
-                    else printf("NIE\n");
-        if (bufor2 == 1) printf("TAK\n");
-                    else printf("NIE\n");
+
+
+
+
 
 return 0;
 
